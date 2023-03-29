ee06910470d21cff42f9be6f27caa54c8656ec52
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-03-29 14:54:14 +0200

    Add named URL pattern for agent detail view

--- agency/urls.py
+++ agency/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from agency.views import index, AgentListView
+from agency.views import index, AgentListView, AgentDetailView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("agents/", AgentListView.as_view(), name="agent-list")
+    path("agents/", AgentListView.as_view(), name="agent-list"),
+    path("agents/<int:pk>/", AgentDetailView.as_view(), name="agent-detail")
 ]
 
 app_name = "agency"
