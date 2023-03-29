ddd71d8913fe12a1adf65bde9184abae31eed522
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-03-29 18:56:38 +0200

    Add path for creation agent to urls

--- agency/urls.py
+++ agency/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
-from agency.views import index, AgentListView, AgentDetailView, PropertyListView, PropertyDetail
+from agency.views import (
+    index,
+    AgentListView,
+    AgentDetailView,
+    PropertyListView,
+    PropertyDetail,
+    AgentCreateView
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("agents/", AgentListView.as_view(), name="agent-list"),
     path("agents/<int:pk>/detail/", AgentDetailView.as_view(), name="agent-detail"),
+    path("agents/create/", AgentCreateView.as_view(), name="agent-create"),
     path("properties/", PropertyListView.as_view(), name="property-list"),
     path("properties/<int:pk>/detail/", PropertyDetail.as_view(), name="property-detail")
 ]
