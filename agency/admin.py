214980927f39f3a7e4879c1d75208e183086e97d
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-03-29 12:57:33 +0200

    Add admin.py with registered models

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from agency.models import Agent, Area, Property, Client
+
+
+@admin.register(Agent)
+class AgentAdmin(UserAdmin):
+    search_fields = ("last_name",)
+
+
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    filter = ("price", "area",)
+    search_fields = ("area",)
+    list_display = ("title", "price", "area",)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+    search_fields = ("last_name",)
+    filter = ("is_searching_for_property",)
