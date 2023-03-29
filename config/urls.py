7a70c14bbed349ed88ff713924153a1d6bff4125
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-03-29 17:20:08 +0200

    Add url for login and logout in root directory

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("agency.urls", namespace="agency"))
+    path("", include("agency.urls", namespace="agency")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
