f93d37e404889cfc115f7c0812d3d0634690e730
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-04-04 13:46:45 +0200

    Change DEBUG in settings

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
-DEBUG = "RENDER" not in os.environ
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
