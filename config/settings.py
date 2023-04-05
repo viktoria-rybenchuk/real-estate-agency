c8a0876a0c697a0c17076bfb5d1769ccd3553a0d
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-04-06 01:44:06 +0200

    Add real-agency.onrender.com to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -29,9 +28,8 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 
 ALLOWED_HOSTS = ["127.0.0.1",
-                 "real-agency.onrender.com/"
+                 "real-agency.onrender.com"
                  ]
-
 
 # Application definition
 
@@ -81,7 +79,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -126,7 +123,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = "staticfiles/"
@@ -134,7 +130,6 @@ STATIC_ROOT = "staticfiles/"
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
